338499167e9a0127573ffe2aff0edc51e0b7987c
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-22 20:12:46 +0530

    accessing the elements

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
-const canvas = document.querySelector("#canvas")
+// all things are coming to the this array like rec and all
+let elements = []
 
-console.log(canvas);
+let selectelm = null;
+
+let isDragging = false;
+
+let isResizing = false;
+
+let nextId = 1;
+
+let resizeHandle = null;
+let elementStartX = 0;
+let elementStartY = 0;
+let elementStartWidth = 0;
+let elementStartHeight = 0;
+let currentOpacity = 1;
+
+
+const canvas = document.getElementById('canvas');
+
+// reload hone pr means load hote hai isme set ho jayegs
+window.addEventListener('load', () => {
+    loadDesign();
+});
+
