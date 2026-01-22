7ee60818639b56a401d3ced13053bb1a68cb8c70
dhruval1907 <dhruvaldalwadi1907@gmail.com>
2026-01-22 20:15:14 +0530

    rectangle partdone

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ window.addEventListener('load', () => {
     loadDesign();
 });
 
+// rectangle banne ke liye
+
+function createREc() {
+    const element = {
+        id: 'element-' + nextId++,
+        type: 'rectangle',
+        x: 100,
+        y: 100,
+        width: 200,
+        height: 150,
+        rotation: 0,
+        backgroundColor: '#3498db',
+        opacity: currentOpacity,
+        zIndex: elements.length
+    }
+    element.push(element)
+    renderElem(element)
+    selectElement(element);
+    updateLayers();
+    saveDesign();
+}
